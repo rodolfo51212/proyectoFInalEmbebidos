086abf41040d8466acc4896cfa1a943450ebb97c
leonlimnes <francotzar@yahoo.com>
2022-05-26 17:23:24 -0500

    COmpletada funcion loadVIdeo

--- loadVideoIntro.py
+++ loadVideoIntro.py
@@ -1,8 +1,6 @@
 #!/usr/bin/sudo /usr/bin/python3
-
-def loadVideo(fileDir):
-    with open("/etc/rc.local", "w+") as file:
-        data = """#!/bin/sh -e
+def rcLocalConfig(fileDir):
+    config_rcLocal= """#!/bin/sh -e
 #
 # rc.local
 #
@@ -22,9 +20,92 @@ if [ "$_IP" ]; then
 fi
 
 #Run video on boot
-cvlc {} &
+omxplayer {} &
 exit 0
 """.format(fileDir)
-        file.write(data)
-loadVideo("/home/pi/Desktop/ProyectoFinal/VIdeoIntro/logo2.mov")
+    with open("/etc/rc.local", "w+") as file_rcLocal:
+        file_rcLocal.write(config_rcLocal)
+        
+def bootConfig():
+    config = """# For more options and information see
+# http://rpf.io/configtxt
+# Some settings may impact device functionality. See link above for details
+
+# uncomment if you get no picture on HDMI for a default "safe" mode
+#hdmi_safe=1
+
+# uncomment this if your display has a black border of unused pixels visible
+# and your display can output without overscan
+#disable_overscan=1
+
+# uncomment the following to adjust overscan. Use positive numbers if console
+# goes off screen, and negative if there is too much border
+#overscan_left=16
+#overscan_right=16
+#overscan_top=16
+#overscan_bottom=16
+
+# uncomment to force a console size. By default it will be display's size minus
+# overscan.
+#framebuffer_width=1280
+#framebuffer_height=720
+
+# uncomment if hdmi display is not detected and composite is being output
+#hdmi_force_hotplug=1
+
+# uncomment to force a specific HDMI mode (this will force VGA)
+#hdmi_group=1
+#hdmi_mode=1
+
+# uncomment to force a HDMI mode rather than DVI. This can make audio work in
+# DMT (computer monitor) modes
+#hdmi_drive=2
+
+# uncomment to increase signal to HDMI, if you have interference, blanking, or
+# no display
+#config_hdmi_boost=4
+
+# uncomment for composite PAL
+#sdtv_mode=2
+
+#uncomment to overclock the arm. 700 MHz is the default.
+#arm_freq=800
+
+# Uncomment some or all of these to enable the optional hardware interfaces
+#dtparam=i2c_arm=on
+#dtparam=i2s=on
+#dtparam=spi=on
+
+# Uncomment this to enable infrared communication.
+#dtoverlay=gpio-ir,gpio_pin=17
+#dtoverlay=gpio-ir-tx,gpio_pin=18
+
+# Additional overlays and parameters are documented /boot/overlays/README
+
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+
+[pi4]
+# Enable DRM VC4 V3D driver on top of the dispmanx display stack
+dtoverlay=vc4-fkms-v3d
+max_framebuffers=2
+
+[all]
+#dtoverlay=vc4-fkms-v3d
+disable_splash=1
+ """
+    with open("/boot/config.txt", "w+") as file_bootConfig:
+        file_bootConfig.write(config)
+
+def cdmlineConfig():
+    config = """console=serial0,115200 console=tty1 root=PARTUUID=2962f868-02 rootfstype=ext4 fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles consoleblank=1 logo.nologo quiet loglevel=0 plymouth.enable=0 vt.global_cursor_default=0 plymouth.ignore-serial-consoles splash fastboot noatime nodiratime noram
+"""
+    with open("//boot/cmdline.txt", "w+") as file_cdmConfig:
+        file_cdmConfig.write(config)
+def loadVideo(fileDir):
+    rcLocalConfig(fileDir)
+    bootConfig()
+    cdmlineConfig()
+    
+loadVideo("/home/pi/Desktop/proyectoFInalEmbebidos/VIdeoIntro/logo.mp4")
     
